d433964a094aa62f53410e28f3cefff195314402
Shad-islam <shadislam48@gmail.com>
2023-09-28 02:47:20 +0600

    Add js funtions

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("cntrlrBtn");
+const promodoroButton = document.getElementById("promodoro");
+const shortBreakButton = document.getElementById("shortBreak");
+const longBreakButton = document.getElementById("longBreak");
+
+let timer;
+let timeInSeconds = 1500; 
+let isTimerRunning = false;
+
+function updateDisplay() {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  timerDisplay.textContent = `${String(minutes).padStart(2, "0")} : ${String(
+    seconds
+  ).padStart(2, "0")}`;
+}
+
+function startTimer() {
+  if (!isTimerRunning) {
+    isTimerRunning = true;
+    startButton.textContent = "Pause";
+    timer = setInterval(() => {
+      if (timeInSeconds > 0) {
+        timeInSeconds--;
+        updateDisplay();
+      } else {
+        clearInterval(timer);
+        isTimerRunning = false;
+        startButton.textContent = "Start";
+      }
+    }, 1000);
+  } else {
+    clearInterval(timer);
+    isTimerRunning = false;
+    startButton.textContent = "Start";
+  }
+}
+
+startButton.addEventListener("click", startTimer);
+
+promodoroButton.addEventListener("click", () => {
+  if (!isTimerRunning) {
+    timeInSeconds = 1500; 
+    updateDisplay();
+  }
+});
+
+shortBreakButton.addEventListener("click", () => {
+  if (!isTimerRunning) {
+    timeInSeconds = 300; 
+    updateDisplay();
+  }
+});
+
+longBreakButton.addEventListener("click", () => {
+  if (!isTimerRunning) {
+    timeInSeconds = 900; 
+    updateDisplay();
+  }
+});
+
+// Initial display
+updateDisplay();
